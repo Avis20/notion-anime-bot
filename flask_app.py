acb280b12ee37e51bdd55df94f8551d1e488d3b2
Орлов Ярослав <y.orlov@fonmix.ru>
2021-06-16 19:57:52 +0300

    'master: init commit Ср 16 июн 2021 19:57:52 MSK'

--- flask_app.py
+++ flask_app.py
@@ -1,63 +1,31 @@
-
-import logging
-import logging.handlers as handlers
-import uuid
-
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import ContentTypes
-from aiogram.utils.executor import start_webhook
-
+from flask import Flask, request
+import telepot
+import urllib3
 import utils
 
-# webhook settings
-WEBHOOK_HOST = 'https://yorlov.pythonanywhere.com'
-WEBHOOK_PATH = f'/path/to/api/{uuid.uuid4()}'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-# webserver settings
-WEBAPP_HOST = 'localhost'  # or ip
-WEBAPP_PORT = 3001
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(
+    proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 
-# logger
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger()
-log_handler = handlers.RotatingFileHandler(utils.get_log_name(), maxBytes=5*1024, backupCount=2)
-logger.addHandler(log_handler)
+TOKEN = utils.get_config('telegram', 'BOT_TOKEN')
+bot = telepot.Bot(TOKEN)
+bot.setWebhook("https://yorlov.pythonanywhere.com/webhook", max_connections=1)
 
-config = utils.get_config()
-print(config)
-TOKEN = config.get('telegram', 'BOT_TOKEN')
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
-dp.middleware.setup(LoggingMiddleware(logger=logger))
+app = Flask(__name__)
 
 
-@dp.message_handler(content_types=ContentTypes.TEXT)
-async def echo_msg(message: types.message):
-    await message.reply(message.text)
-
-
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-
-
-async def on_shutdown(dp):
-    logging.warning('Shutting down..')
-    await bot.delete_webhook()
-    await dp.storage.close()
-    await dp.storage.wait_closed()
-    logging.warning('Bye!')
-
-
-if __name__ == '__main__':
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+@app.route('/webhook', methods=["POST"])
+def telegram_webhook():
+    update = request.get_json()
+    if "message" in update:
+        chat_id = update["message"]["chat"]["id"]
+        if "text" in update["message"]:
+            text = update["message"]["text"]
+            bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+        else:
+            bot.sendMessage(chat_id, "From the web: sorry, I didn't understand that kind of message")
+    return "OK"
--- utils.py
+++ utils.py
@@ -5,16 +5,21 @@ from pathlib import Path
 
 # BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
+
 def get_log_name():
     log_dir = Path.home() / 'logs'
     if not log_dir.exists():
         os.makedirs(log_dir)
     return log_dir / 'notion-tg-bot.log'
 
+
 def get_config():
     config = configparser.ConfigParser()
-    config.read('/home/yorlov/mysite/.config')
+    config.read(Path.home() / 'mysite' / '.config')
     return config
 
+
 if __name__ == '__main__':
-    get_config()
+    config = get_config()
+    print(config)
+    print(config.get('telegram', 'BOT_TOKEN'))
