d04a211ef87f4babc35aa32cbf40a7e7ff851817
Орлов Ярослав <y.orlov@fonmix.ru>
2021-06-19 11:28:04 +0300

    'master: init commit Сб 19 июн 2021 11:28:04 MSK'

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,9 @@ import logging
 import os
 from logging import handlers
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.dispatcher.webhook import SendMessage
-from aiogram.utils.executor import start_webhook
+from aiogram.types import ContentTypes
 
 from flask import Flask
 
@@ -18,11 +17,6 @@ logger.addHandler(log_handler)
 
 config = utils.get_config()
 TOKEN = config.get('telegram', 'bot_token')
-WEBHOOK_HOST = config.get('site', 'webhook_host')
-WEBHOOK_PATH = '/webhook'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-WEBAPP_HOST = '127.0.0.1'
-WEBAPP_PORT = config.get('site', 'webhook_port')
 
 bot = None
 if not os.environ.get('PROXY_OFF'):
@@ -42,31 +36,11 @@ def echo():
     return 'hello'
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    return SendMessage(message.chat.id, message.text)
-
-
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-
-
-async def on_shutdown(dp):
-    logging.warning('Shutting down..')
-    await bot.delete_webhook()
-    await dp.storage.close()
-    await dp.storage.wait_closed()
-    logging.warning('Bye!')
+@dp.message_handler(content_types=ContentTypes.TEXT)
+async def echo_msg(message: types.message):
+    await message.reply(message.text)
 
 
 if __name__ == '__main__':
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    executor.start_polling(dp)
     app.run()
